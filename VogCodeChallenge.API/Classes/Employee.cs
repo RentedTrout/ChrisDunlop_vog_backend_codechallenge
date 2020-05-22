a48c46ed1c67f5999b4e503b6d46a63dfe990741
Chris Dunlop <rentedtrout@gmail.com>
2020-05-22 13:31:36 -0600

    Rename Solution Name

---
+++ VogCodeChallenge.API/Classes/Employee.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace ChrisDunlop_vog_backend_codechalleneg.Classes
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string JobTitle { get; set; }
+
+        public string MailingAddress { get; set; }
+
+    }
+}
